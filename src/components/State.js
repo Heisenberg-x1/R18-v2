7f9acc4014cd5b8e8e1f83f14a15e926f4daa1be
Heisenberg-x1 <chemerilcollins@gmail.com>
2023-06-27 21:36:19 +0200

    Readme

--- src/components/State.js
+++ src/components/State.js
@@ -2,23 +2,24 @@ import React from "react";
 import { useState } from "react";
 
 const styles = {
-    border:"1px solid teal",
-    padding:"10px",
-    margin:"10px",
-    width:"fit-content",
-    borderRadius: "5px"}
+  border: "1px solid teal",
+  padding: "10px",
+  margin: "10px",
+  width: "fit-content",
+  borderRadius: "5px",
+};
 export default function State() {
-    // An array of 6 friends
-    const friends = ["John", "Jane", "Jack", "Jill", "Joe", "Jenny"];
+  // An array of 6 friends
+  const friends = ["John", "Jane", "Jack", "Jill", "Joe", "Jenny"];
   const [name, setName] = useState(friends[0]);
   const [age, setAge] = useState(25);
   const change = () => {
-let random = Math.floor(Math.random() * friends.length);
+    let random = Math.floor(Math.random() * friends.length);
     setName(friends[random]);
-    setAge(Math.ceil(Math.random()*17));
+    setAge(Math.ceil(Math.random() * 17));
   };
   return (
-<div className="State" style={styles}>
+    <div className="State" style={styles}>
       <button type="button" onClick={change}>
         Click
       </button>
